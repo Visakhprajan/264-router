7e39d9fdf0774bd37f464b7b7729382dc84dedf6
Visakhprajan <visakhprajan@gmail.com>
2021-06-09 19:43:52 +0530

    router finished

---
+++ src/pages/Products.js
@@ -0,0 +1,14 @@
+const Products = () => {
+return(
+  <section>
+    <h1>The Products Page</h1>
+    <ul>
+      <li>Books</li>
+      <li>Carpet</li>
+      <li>Online course</li>
+    </ul>
+  </section>
+)
+}
+
+export default Products;
